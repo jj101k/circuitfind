00810d63b36441a0d2ed5f1838e280feb2cc87fc
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-10-01 15:12:41 +0100

    A little bit of abstraction

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 "use strict"
 
+class PositionedNode {
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {?string} [colour]
+     */
+    constructor(x, y, colour = null) {
+        this.x = x
+        this.y = y
+        this.colour = colour
+    }
+    display(ctx) {
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.fillStyle = this.colour
+        ctx.fillRect(0.1, 0.1, 0.8, 0.8)
+        ctx.restore()
+    }
+}
+
 var c = document.getElementById("grid")
 if(c instanceof HTMLCanvasElement) {
     var ctx = c.getContext("2d")
@@ -19,15 +40,28 @@ if(c instanceof HTMLCanvasElement) {
     ctx.lineWidth = 2 / cw
     ctx.stroke()
 
+    let obstructions = []
     for(let i = 0; i < 31; i++) {
-        let cx = Math.floor(Math.random() * 10)
-        let cy = Math.floor(Math.random() * 10)
-        ctx.save()
-        ctx.translate(cx, cy)
-        ctx.fillStyle = "red"
-        ctx.fillRect(0.1, 0.1, 0.8, 0.8)
-        ctx.restore()
+        obstructions.push(new PositionedNode(
+            Math.floor(Math.random() * 10),
+            Math.floor(Math.random() * 10),
+            "red"
+        ))
     }
+
+    let start = new PositionedNode(
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+        "green"
+    )
+    let finish = new PositionedNode(
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+        "blue"
+    )
+    obstructions.forEach(o => o.display(ctx))    
+    start.display(ctx)
+    finish.display(ctx)
 } else {
     console.log("Well, that's the wrong element type")
 }
